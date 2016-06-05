4b41171bc1e147cd7f51dc2ad2660fd87584d7c8
vlad9508 <vladimir_xb_x3@hotmail.com>
2016-06-04 21:30:00 -0500

    Add files via upload

---
+++ marca.sql
@@ -0,0 +1,7 @@
+INSERT INTO marca VALUES( '001', 'Audi', 'Alta');
+INSERT INTO marca VALUES( '010', 'Bmw', 'Alta');
+INSERT INTO marca VALUES( '011', 'Renault', 'Media');
+INSERT INTO marca VALUES( '100', 'Bodge', 'Baja');
+INSERT INTO marca VALUES( '101', 'volkswagen', 'Baja');
+INSERT INTO marca VALUES( '110', 'Toyota', 'Media');
+INSERT INTO marca VALUES( '111', ´Lincon', 'Alta');
