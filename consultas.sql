2dbbfe68f8ee908c6834c9adf0e34c28d7b769f2
Dano <j.danoxd@outlook.es>
2016-06-16 16:44:34 -0500

    2da pregunta bien

--- consultas.sql
+++ consultas.sql
@@ -4,14 +4,18 @@ INNER JOIN STOCK
 on STOCK.ID_DIS = DISTRIBUIDOR.ID_DIS
 WHERE STOCK.ID_CAR = 963;
 
-SELECT stock.cantidad AS "cantidad_jetta", distribuidor.nombre, carro.color
-FROM distribuidor JOIN stock JOIN carro
-USING (stock.ID_carro)
-WHERE stock.ID_carro = "10101" AND
-	  carro.color = "blanco" AND
-	  carro.color = "negro" AND
-	  distribuidor.nombre = "x" AND
-	  distribuidor.nombre = "y";
+SELECT STOCK.CANTIDAD AS "cantidad_jetta", DISTRIBUIDOR.NOMBRE, car.color
+FROM distribuidor 
+inner JOIN stock 
+on STOCK.ID_DIS = DISTRIBUIDOR.ID_DIS
+inner JOIN car
+on STOCK.ID_car = car.ID_car
+WHERE stock.ID_car = 963 OR
+	  stock.ID_car = 753 OR
+	  car.color = 'cafe' OR
+	  car.color = 'violeta' AND
+	  distribuidor.nombre = 'Automotors' OR
+	  distribuidor.nombre = 'Motorsort';
 
 SELECT carro.nombre, distribuidor.nombre
 FROM distribuidor
