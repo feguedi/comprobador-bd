474fd8929c5b356e18575025c6d682a5eab85ffe
Eduardo Fernández <feguedi@yahoo.com.mx>
2016-06-17 06:33:59 -0500

    Comprobando la pregunta

--- comprobador/comparar.py
+++ comprobador/comparar.py
@@ -1,3 +1,4 @@
+from PyQt5.QtWidgets import QMessageBox
 import base_datos.preguntas as preguntas
 
 
@@ -13,7 +14,7 @@ def comp(contador, cons_res):
         if res1.sort() == res2.sort():
             res1.clear()
             res2.clear()
-            return True
+            msj_comprobacion(True)
 
     elif contador == 2:
         for i in preguntas.pregunta_2():
@@ -23,7 +24,7 @@ def comp(contador, cons_res):
         if res1.sort() == res2.sort():
             res1.clear()
             res2.clear()
-            return True
+            msj_comprobacion(True)
 
     elif contador == 3:
         for i in preguntas.pregunta_3():
@@ -33,7 +34,7 @@ def comp(contador, cons_res):
         if res1.sort() == res2.sort():
             res1.clear()
             res2.clear()
-            return True
+            msj_comprobacion(True)
 
     elif contador == 4:
         for i in preguntas.pregunta_4():
@@ -43,7 +44,16 @@ def comp(contador, cons_res):
         if res1.sort() == res2.sort():
             res1.clear()
             res2.clear()
-            return True
+            msj_comprobacion(True)
 
     else:
+        msj_comprobacion(False)
+
+
+def msj_comprobacion(valor_res):
+    if valor_res:
+        QMessageBox.about(None, 'Correcto', 'Tu respuesta es correcta', QMessageBox.Close)
+        return True
+    elif not valor_res:
+        QMessageBox.about(None, 'Incorrecto', 'Tu respuesta es incorrecta. Inténtalo nuevamente', QMessageBox.Close)
         return False
--- comprobador/comprobador.py
+++ comprobador/comprobador.py
@@ -1,3 +1,5 @@
+from PyQt5.QtWidgets import QMessageBox
+
 # Objeto x debe ser una lista
 def texto(x):
     iterador = len(x)
@@ -5,8 +7,18 @@ def texto(x):
     for i in range(iterador):
         print("Evaluando:", x[i])
         if x[i] is "DELETE" or x[i] is "ALTER" or x[i] is "DROP" or x[i] is "CREATE":
-            return False
+            mensaje(False)
         elif x[i] is "delete" or x[i] is "alter" or x[i] is "drop" or x[i] is "create":
-            return False
+            mensaje(False)
         else:
             return True
+
+
+def mensaje(valor):
+    if not valor:
+        QMessageBox.about(None, 'Instrucción incorrecta',
+                          'Ingrese las instrucciones debidas dentro de las convenciones'
+                          ' de código de PL/SQL.\n'
+                          'No ingrese instrucciones para insertar, borrar o crear datos'
+                          ' o tablas.',
+                          QMessageBox.Close)
